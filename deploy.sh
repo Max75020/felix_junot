d991d05b5baf67477ccdc2a5a10c0f00f1e2c531
Max75020 <maxime.duplaissy@gmail.com>
2024-11-17 12:52:43 +0000

    ajout du script de déploiment sur le serveur

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Chemin vers votre projet
+PROJECT_DIR="/root/var/www/felix_junot"
+
+# Naviguer dans le dossier du projet
+cd "$PROJECT_DIR" || exit
+
+# Récupérer les dernières modifications depuis GitHub
+echo ">>> Récupération des dernières modifications depuis GitHub..."
+git pull origin main
+
+# Construire et redémarrer les conteneurs Docker
+echo ">>> Construction et redémarrage des conteneurs Docker..."
+docker-compose down
+docker-compose up -d --build
+
+echo ">>> Les conteneurs Docker sont en cours d'exécution."
+
+# Vérifier les logs pour détecter d'éventuelles erreurs
+#echo ">>> Les conteneurs Docker sont en cours d'exécution. Vérification des logs..."
+#docker-compose logs -f
+
+# Ligne pour quitter la visualisation des logs après exécution
+#echo ">>> Appuyez sur Ctrl+C pour quitter les logs à tout moment."
